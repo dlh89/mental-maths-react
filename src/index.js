5826ec39022bdce97081e22f41d67961e75edb05
David Hide <d.hide@live.co.uk>
2024-04-28 12:17:27 +0100

    Remove StrictMode to prevent checkbox triggers firing twice in development

--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
-  </React.StrictMode>
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
