36dcebeb38b065d5faf189fa1f2b1268af305822
David Hide <d.hide@live.co.uk>
2024-06-02 14:23:53 +0100

    Handle rendering questions, answers and score

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -5,12 +5,21 @@ import { ReactComponent as Tick } from '../images/tick.svg';
 import { ReactComponent as Cross } from '../images/cross.svg';
 import { ReactComponent as StopwatchWhite } from '../images/stopwatch-white.svg';
 import { ReactComponent as StopwatchBlack } from '../images/stopwatch.svg';
+import {
+    getRandomElement,
+    getDigitsArr,
+    getSymbol,
+    getAnswer
+} from '../utils/helpers';
 
 const Play = () => {
     const [searchParams] = useSearchParams();
-    useEffect(() => {
-        // TODO process get params
-    }, []);
+    const [questionTypes, setQuestionTypes] = useState(searchParams.getAll('question_types'));
+    const [multiplicationDigits, setMultiplicationDigits] = useState(searchParams.getAll('multiplication_digits'));
+    const [additionDigits, setAdditionDigits] = useState(searchParams.getAll('addition_digits'));
+    const [subtractionDigits, setSubtractionDigits] = useState(searchParams.getAll('subtraction_digits'));
+    const [includeSubtractionNegatives, setIncludeSubtractionNegatives] = useState(searchParams.getAll('include_negatives'));
+    const [repeatIncorrectQuestions, setRepeatIncorrectQuestions] = useState(searchParams.getAll('repeat_incorrect_questions'));
 
     const [score, setScore] = useState({
         answers: [],
@@ -24,8 +33,13 @@ const Play = () => {
     const [timer, setTimer] = useState(0);
     const timerRef = useRef(null);
     const [question, setQuestion] = useState(false);
+    const [queue, setQueue] = useState([]);
+    const [questionsSinceRepeat, setQuestionsSinceRepeat] = useState(0);
+    const [questionIsAnswered, setQuestionIsAnswered] = useState(false);
+    const [answer, setAnswer] = useState(false);
 
     const startTimer = () => {
+        setTimer(0);
         if (timerRef.current !== null) return; // Prevent multiple timers
         timerRef.current = setInterval(() => {
           setTimer(prevTime => prevTime + 1);
@@ -37,10 +51,161 @@ const Play = () => {
         timerRef.current = null;
     };
 
-    const handleAnswer = () => {
-        stopTimer();
-        // TODO update score, show mark buttons
+    const nextQuestion = () => {
+        if (queue.length) {
+            if (questionsSinceRepeat >= 1) {
+                setQuestion(queue.shift());
+                setQueue((prevItems) => prevItems.slice(1));
+                if (!queue.length) {
+                    setQuestionsSinceRepeat(0);
+                }
+            } else {
+                getNewQuestion();
+                setQuestionsSinceRepeat(questionsSinceRepeat + 1);
+            }
+        } else {
+            getNewQuestion();
+        }
+
+        setQuestionIsAnswered(false);
+        startTimer();
     }
+
+    const getNewQuestion = () => {
+        const type = getRandomElement(questionTypes);
+        const arithmeticTypeDigits = getArithmeticTypeDigits(type);
+        const numDigits = getRandomElement(arithmeticTypeDigits);
+        const digitsArr = getDigitsArr(numDigits);
+        const question = generateQuestion(type, ...digitsArr);
+        if (type === 'subtraction' && !includeSubtractionNegatives) {
+            if (question.second > question.first) {
+                // Re-order the question so the largest number is on the left hand side
+                const tempSecond = question.second;
+                question.second = question.first;
+                question.first = tempSecond;
+            }
+        }
+        setQuestion(question);
+    }
+
+    const getArithmeticTypeDigits = (type) => {
+        let arithmeticTypeDigits = false;
+
+        switch (type) {
+            case 'multiplication':
+                arithmeticTypeDigits = multiplicationDigits;
+                break;
+            case 'addition':
+                arithmeticTypeDigits = additionDigits;
+                break;
+            case 'subtraction':
+                arithmeticTypeDigits = subtractionDigits;
+                break;
+            default:
+                break;
+        }
+
+        return arithmeticTypeDigits;
+    }
+
+    const generateQuestion = (type, firstNumDigits, secondNumDigits) => {
+        const firstNumExcludeNums = getExcludeNums(type, firstNumDigits);
+        const secondNumExcludeNums = getExcludeNums(type, secondNumDigits);
+        const first = getNumber(firstNumDigits, firstNumExcludeNums);
+        const second = getNumber(secondNumDigits, secondNumExcludeNums);
+    
+        const question = {
+            'first': first,
+            'second': second,
+            'type': type,
+            'firstNumDigits': firstNumDigits,
+            'secondNumDigits': secondNumDigits,
+        }
+    
+        return question;
+    }
+
+    /**
+     * Return an array of numbers that can't be included for the number of digits
+     * @param {string} type 
+     * @param {string|integer} numDigits 
+     * @returns {array}
+     */
+    const getExcludeNums = (type, numDigits) => {
+        const excludeNums = [];
+        if (parseInt(numDigits) === 1) {
+            // Avoid questions such as 0 + 0
+            excludeNums.push(0);
+    
+            if (type === 'multiplication') {
+                // Avoid questions such as 1 x 1
+                excludeNums.push(1);
+            }
+        }
+    
+        return excludeNums;
+    }
+    
+    /**
+     * Return a random number with the number of digits given
+     * 0 will not be used for the first digit and any number in excludeNums will not be used
+     * @param {integer} numDigits 
+     * @param {array} excludeNums 
+     * @returns {integer}
+     */
+    const getNumber = (numDigits, excludeNums = []) => {
+        let number = '';
+    
+        for (let i = 0; i < numDigits; i++) {
+            let randomDigit = getRandomDigit();
+            while ((i === 0 && numDigits > 1 && randomDigit === 0) || excludeNums.includes(randomDigit)) {
+                randomDigit = getRandomDigit();
+            }
+            number += randomDigit;
+        }
+    
+        return parseInt(number);
+    }
+
+    /**
+     * Return a random number between 0 and 9
+     * @returns {integer}
+     */
+    const getRandomDigit = () => {
+        return Math.round(Math.random() * 9);
+    }
+
+    const handleAnswer = () => {
+        setQuestionIsAnswered(true);
+        stopTimer();
+        setAnswer(getAnswer(question));
+    }
+
+    const handleMark = (isCorrect) => {
+        const answeredQuestion = {
+            ...question,
+            timeToAnswer: timer,
+            isCorrect,
+        }
+
+        setScore(prevItems => ({
+            ...prevItems,
+            answers: [...prevItems.answers, answeredQuestion],
+            correct: isCorrect ? prevItems.correct + 1 : prevItems.correct,
+            incorrect: !isCorrect ? prevItems.incorrect + 1 : prevItems.incorrect
+        }));
+
+        if (!isCorrect && repeatIncorrectQuestions) {
+            setQueue((prevItems) => [...prevItems, question]);
+        }
+        nextQuestion();
+    }
+
+    useEffect(() => {
+        // Start game
+        setScore(prev => ({...prev, startTime: new Date().getTime()}))
+        nextQuestion();
+    }, []);
 
     return (
         <div>
@@ -62,11 +227,20 @@ const Play = () => {
                     <div className="question">
                         {question && (
                             <Fragment>
-                                {question} = { question.answer && (<span className="question__answer">{question.answer}</span>) }
+                                {question.first} {getSymbol(question.type)} {question.second} = { questionIsAnswered && (<span className="question__answer">{answer}</span>) }
                             </Fragment>
                         )}
                     </div>
-                    <button className="btn btn-primary btn-lg" onClick={handleAnswer}>Answer</button>
+                    {!questionIsAnswered && (
+                        <button className="btn btn-primary btn-lg" onClick={handleAnswer}>Answer</button>
+                    )}
+                    {questionIsAnswered && (
+                        <div>
+                            <p>Did you get the correct answer?</p>
+                            <button className="btn btn-lg btn-success" onClick={(() => handleMark(true))}><Tick className="badge-icon"/> Yes</button>
+                            <button className="btn btn-lg btn-danger" onClick={(() => handleMark(false))}><Cross className="badge-icon"/> No</button>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
---
+++ src/utils/helpers.js
@@ -0,0 +1,191 @@
+/**
+ * Get the symbol for the given mathematical string
+ * @param {string} type 
+ * @returns {string}
+ */
+export const getSymbol = (type) => {
+    let symbol;
+
+    switch (type) {
+        case 'multiplication':
+            symbol = 'x';
+            break;
+        case 'addition':
+            symbol = '+';
+            break;
+        case 'subtraction':
+            symbol = '-';
+            break;
+        default:
+            break;
+    }
+
+    return symbol;
+}
+
+/**
+ * Return a random element from the given array
+ * @param {array} arr 
+ * @returns {*}
+ */
+export const getRandomElement = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+/**
+ * Translate string into array of two integers
+ * @param {string} numDigits 
+ */
+export const getDigitsArr = (numDigits) => {
+    return numDigits.split('x');
+}
+
+/**
+ * Return the answer for the given question
+ * @param {object} question 
+ * @returns {integer}
+ */
+export const getAnswer = (question) => {
+    let answer;
+
+    switch (question.type) {
+        case 'multiplication':
+            answer = question.first * question.second
+            break;
+        case 'addition':
+            answer = question.first + question.second
+            break;
+        case 'subtraction':
+            answer = question.first - question.second
+            break;
+        default:
+            break;
+    }
+
+    return answer;
+}
+
+export const getDistanceToNearestTen = (n) => {
+    let distanceToTen;
+
+    if (n > 5) {
+        distanceToTen = 10 % n;
+    } else {
+        distanceToTen = n % 10;
+    }
+
+    return distanceToTen;
+}
+
+/**
+ * Get zero-indexed digit of a number as an integer
+ *
+ * @param {integer|string} number The number to parse
+ * @param {integer|string} digit Zero-indexed digit to get
+ * @returns {integer}
+ */
+export const getDigit = (number, digit) => {
+    return parseInt(number.toString()[digit]);
+}
+
+export const swapQuestion = (question) => {
+    const tempSecond = question.second;
+    question.second = question.first;
+    question.first = tempSecond;
+
+    return question;
+}
+
+ /**
+ * Return a string of the question and answer, e.g. "70 x 66 = 4620"
+ * @param {object} question 
+ * @returns {string}
+ */
+export const getQuestionAndAnswerText = (question) => {
+    const answer = this.getAnswer(question);
+
+    return `${question.first} ${this.getSymbol(question.type)} ${question.second} = ${answer}`;
+}
+
+/**
+ * Build a string of text with a line for each question and answer, skipping questions where operands are 0 or 1
+ * @param {array} questions 
+ * @returns {string}
+ */
+export const buildHelpText = (questions) => {
+    let helpText = '';
+    const skipOperands = [0, 1];
+
+    for (const question of questions) {
+        if (skipOperands.includes(question.first) || skipOperands.includes(question.second)) {
+            continue;
+        }
+
+        helpText += '\n' + this.getQuestionAndAnswerText(question);
+    }
+
+    return helpText;
+}
+
+export const isInArray = (arr, searchItem) => {
+    return arr.some(item => JSON.stringify(item) === JSON.stringify(searchItem));
+}
+
+export const getPercentageString = (numerator, denominator) => {
+    return Math.round(numerator / denominator * 100) + '%';
+}
+
+export const getFormattedMilliseconds = (timeInMilliseconds) => {
+    const hours = Math.floor(timeInMilliseconds / (1000 * 60 * 60));
+    timeInMilliseconds = timeInMilliseconds % (1000 * 60 * 60);
+  
+    const minutes = Math.floor(timeInMilliseconds / (1000 * 60));
+    timeInMilliseconds = timeInMilliseconds % (1000 * 60);
+  
+    const seconds = Math.floor(timeInMilliseconds / 1000);
+  
+    // Format numbers as two-digit strings
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+  
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
+// Return the question type with first char uppercase followed by a space and the subtype
+export const getQuestionTypeLabel = (questionType, questionSubtype) => {
+    return `${questionType.charAt(0).toUpperCase() + questionType.slice(1)} ${questionSubtype}`;
+}
+
+export const addPropertyIfNotExists = (obj, prop, addType = 'arr') => {
+    if (!obj.hasOwnProperty(prop)) {
+        switch (addType) {
+            case 'arr':
+                obj[prop] = [];
+                break;
+            case 'obj':
+                obj[prop] = {};
+                break;
+        
+            default:
+                obj[prop] = false;
+                break;
+        }
+    }
+
+    return obj;
+}
+
+export const getResultsByQuestionType = (results) => {
+    let resultsByQuestionType = {};
+    results.answers.forEach((answer) => {
+        answer.date = new Date(results.startTime).toLocaleDateString('en-GB');
+        answer.dateTime = results.startTime;
+        resultsByQuestionType = this.addPropertyIfNotExists(resultsByQuestionType, answer.type, 'obj');
+        answer.numDigits = `${answer.firstNumDigits}x${answer.secondNumDigits}`;
+        resultsByQuestionType[answer.type] = this.addPropertyIfNotExists(resultsByQuestionType[answer.type], answer.numDigits);
+        resultsByQuestionType[answer.type][answer.numDigits].push(answer);
+    });
+
+    return resultsByQuestionType;
+}
